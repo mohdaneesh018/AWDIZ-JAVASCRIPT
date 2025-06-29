aab326c1a2c5c00648e9858277e6c0bc29b28e43
mohdaneesh018 <mdaneesh537@gmail.com>
2025-06-29 12:46:09 +0530

    Updates to Day-3

---
+++ Day-3/index.js
@@ -0,0 +1,32 @@
+// Conditional Statement 
+
+// if
+// else if
+// else
+
+// var age = 85;
+var age = 79;
+
+if(age <= 18){
+    console.log("You have Eligible for LL.");
+}
+
+else if( (age <= 80) && (age > 18) ){
+    console.log("You have Eligible for DL.");
+}
+
+else{
+    console.log("You have not Eligible for DL.")
+}
+
+let userSelectplayer = "Dhoni"
+switch (userSelectplayer){
+    case ("Dhoni"):
+        console.log("Yes Correct");
+        break;
+    case ("Kohli"):
+        console.log("No found");
+        break;
+    default: 
+        console.log("None of these");
+}
